5a1775a07375ae7dcc0540d5791cbfc6ebd5e468
muroiwa <emuroiwa@gmail.com>
2020-03-13 10:06:00 +0200

    blueprint intial commit

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="card card-default">
+            <div class="card-header">About</div>
+            <div class="card-body">
+               About
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <div class="card card-default">
+            <div class="card-header">Home</div>
+            <div class="card-body">
+                Home
+            </div>
+        </div>
+    </div>
+</template>

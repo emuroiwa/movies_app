e212ddf19bbd870383aadd139b708ac763bb0413
muroiwa <emuroiwa@gmail.com>
2020-03-13 10:39:12 +0200

    menu update

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link :to="{name: 'home'}" class="navbar-brand">My blueprint laravel vue</router-link>
+    <router-link :to="{name: 'home'}" class="navbar-brand">Movies App</router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -27,10 +27,10 @@
         </li>
       </ul>
 
-      <!-- <form class="form-inline my-2 my-lg-0">
+      <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form> -->
+        <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+      </form>
     </div>
   </nav>
 </template>
